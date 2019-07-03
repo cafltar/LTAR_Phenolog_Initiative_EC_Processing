b11a86bebc34857624df3001a183bec1e62c6787
Eric <eric.s.russell@wsu.edu>
2019-07-03 13:39:56 -0700

    Update LTAR_Phenology_Initiative_QC_REddy.py
    Changed to correct directory for the function library used.
    Added in extra conditionals for specific issues encountered during the recent run of EC data. Note these issues are for specific sites and possible files.

--- LTAR_Phenology_Initiative_QC_REddy.py
+++ LTAR_Phenology_Initiative_QC_REddy.py
@@ -16,16 +16,16 @@ import numpy as np
 import datetime
 import warnings
 # Change this path to the directory where the LTAR_Flux_QC.py file is located
-os.chdir(r'C:\Users\Eric\Documents\GitHub\LTAR_Phenology_Data_Processing')       
+os.chdir(r'C:\Users\Eric\Documents\GitHub\LTAR_Phenolog_Initiative_EC_Processing')       
 import LTAR_Pheno_QC_Functions as LLT
 import Reddy_Format as REF
-ds = '2018-01-01'
+ds = '2017-01-01'
 de = '2019-01-01'
 
 files = glob.glob('C:\\Users\\Eric\\Desktop\\LTAR\\LTAR_National_Projects\\PhenologyInitiative\\EC Data\\Processed\\Unprocessed\\*.csv') #Directory or file name with file names here
 # File with upper and lower limits for the flux values for each site based on visual inspection of each dataset
 QC = pd.read_csv('C:\\Users\\Eric\\Desktop\\LTAR\\LTAR_National_Projects\\PhenologyInitiative\\QC_Limits_List.csv',header = 0, index_col = 'Site')
-for K in range (0,len(files)):
+for K in range (9,len(files)):
 #Read in data and concat to one dataframe; no processing until data all read in - assumes data is from AmeriFlux or in the format that was defined by the group for data requests
     df = pd.read_csv(files[K],header= 0,sep=',',low_memory=False)
     dt = []
@@ -45,6 +45,7 @@ for K in range (0,len(files)):
         dt = pd.DataFrame(dt);df.index = dt[0]
         df.index=pd.to_datetime(df.index) # Time-based index        
         if 'TimeStamp' in df.columns: df=df.drop(columns = 'TimeStamp') # Specific to JORN
+        if 'HRMIN' in df.columns: df=df.drop(columns = 'HRMIN') # Specific to UCB because formating time is bad
         df = df.astype(float)
     df = LLT.indx_fill(df,'30min') # Fill in and missing half-hours in the dataset to have a continuous data set from start time to end.
     df = df[ds:de] # Limit data to set timeframe based on defined bounds above
@@ -76,15 +77,17 @@ for K in range (0,len(files)):
         E = (data_qc['RH']/100) * Es
         data_qc['VPD'] = Es - E
         print('Calculated VPD')
-    else: data_qc['VPD'] = data_qc[data_qc.filter(like='VPD').columns]; print('Renamed to VPD')
+    else: data_qc['VPD'] = data_qc[data_qc.filter(like='VPD').columns[0]]; print('Renamed to VPD')
     if Site == 'CAF':
         data_qc['VPD'] = data_qc['VPD']/1000
         data_qc['PA'] = data_qc['PA']/1000
         print('Convert CAF to kPa')
     data_qc['VPD'][data_qc['VPD']>35] = np.NaN
-    if 'SW_IN' not in data_qc.columns:
+    if ('SW_IN' not in data_qc.columns) & ('Rg' not in data_qc.columns):
         data_qc['SW_IN'] = data_qc['PPFD_IN']/2.1
         data_qc['SW_IN'][data_qc['SW_IN']< -100] = np.NaN
+    if 'Rg' in data_qc.columns:
+        data_qc['SW_IN'] = data_qc['Rg']
     s = data_qc.index[0]; ss = s
     s+= datetime.timedelta(days=5)
     with warnings.catch_warnings(): #Despike function for turbulent fluxes
